95178a43ef6d85e481bc677df3a8eae93b64ca87
Alexander Shmelev <ashmelev@smartlabs.tv>
2019-10-07 20:07:28 +0300

    IMPR: rewrite to use os/exec instead of slow git-go

--- main.go
+++ main.go
@@ -2,102 +2,128 @@ package main
 
 import (
 	"bufio"
-	"fmt"
-	"os"
-	"strings"
-	"time"
-
 	"container/list"
-
-	"gopkg.in/src-d/go-git.v4"
-	. "gopkg.in/src-d/go-git.v4/_examples"
-	"gopkg.in/src-d/go-git.v4/plumbing"
-	"gopkg.in/src-d/go-git.v4/plumbing/object"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
 )
 
-// Gerrit change-Id prefix in commit message
-const kChangeId = "Change-Id: "
-// Limit log history to speedup 
-const startTime = "2019-01-01T00:00:00Z"
+type Commit struct {
+	ChangeId string
 
-func GerritCommits(r *git.Repository, hash plumbing.Hash) (map[string]*object.Commit, *list.List, error) {
+	CommitId string
+
+	Title string
+}
+
+const kCommit = "commit "
+const kMessagePrefix = "    "
+
+// Gerrit Change-Id: prefix in commit message
+const kChangeId = kMessagePrefix + "Change-Id: "
+
+const filenameGerritCherryIgnore = ".gerrit-cherry-ignore"
+
+func GerritCommits(commitName string) (map[string]*Commit, *list.List, error) {
 	// change-Id -> commit object
-	var m map[string]*object.Commit = make(map[string]*object.Commit)
+	var m map[string]*Commit = make(map[string]*Commit)
 	// ordered changeId
-	var l *list.List = list.New() 
+	var l *list.List = list.New()
 
-	var err error
-
-	since, err := time.Parse(time.RFC3339, startTime)
-	CheckIfError(err)
-
-
-	cIter, err := r.Log(&git.LogOptions{From: hash})
+	cmd := exec.Command("git", "log", "--decorate=short", commitName)
+	stdout, err := cmd.StdoutPipe()
 	if err != nil {
-		return nil, nil, err
+		log.Fatal(err)
+	}
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
 	}
 
-	err = cIter.ForEach(func(c *object.Commit) error {
-		if since.After(c.Committer.When) {
-			return plumbing.ErrInvalidType
-		}
-
-		scanner := bufio.NewScanner(strings.NewReader(c.Message))
-		for scanner.Scan() {
-			line := scanner.Text()
-			if strings.HasPrefix(line, kChangeId) {
-				changeId := strings.TrimPrefix(line, kChangeId)
-				m[changeId] = c
-				l.PushBack(changeId)
-				break
-
+	var commit *Commit
+	scanner := bufio.NewScanner(stdout)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, kCommit) {
+			hash := strings.TrimPrefix(line, kCommit)
+			if idx := strings.IndexByte(hash, ' '); idx >= 0 {
+				hash = hash[:idx]
+			}
+			commit = &Commit{CommitId: hash}
+		} else if strings.HasPrefix(line, kChangeId) {
+			commit.ChangeId = strings.TrimPrefix(line, kChangeId)
+			m[commit.ChangeId] = commit
+			l.PushBack(commit.ChangeId)
+		} else if strings.HasPrefix(line, kMessagePrefix) {
+			if len(commit.Title) == 0 {
+				commit.Title = strings.TrimPrefix(line, kMessagePrefix)
 			}
 		}
-		return nil
-	})
+	}
 
 	return m, l, nil
 }
 
-func PrintCommit(c *object.Commit, changeId string) {
-	short := strings.SplitN(c.Message, "\n", 2)[0]
-
-	fmt.Printf("%v %v %v\n", changeId, c.Hash, short)
+func PrintCommit(c *Commit) {
+	fmt.Printf("%v %v %v\n", c.ChangeId, c.CommitId, c.Title)
 }
 
 func main() {
-	CheckArgs("<commit>")
 	commit := os.Args[1]
 
-	r, err := git.PlainOpen(".")
-	CheckIfError(err)
-
-	refHEAD, err := r.Head()
-	CheckIfError(err)
-	hashHEAD := refHEAD.Hash()
-
-	var refOther *plumbing.Reference
-	// trying reference name as is
-	refOther, err = r.Reference(plumbing.ReferenceName(commit), true)
-	if err != nil {
-		// expanding short reference name to full name
-		for _, format := range plumbing.RefRevParseRules {
-			full := fmt.Sprintf(format, commit)
-			refOther, err = r.Reference(plumbing.ReferenceName(full), true)
-			if err == nil {
-				break
-			}
-		}
-	}
-	CheckIfError(err)
-
 	// read HEAD
-	mapHEAD, _, err := GerritCommits(r, hashHEAD)
-	CheckIfError(err)
+	mapHEAD, _, err := GerritCommits("HEAD")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// read other
-	hashOther := refOther.Hash()
-	mapOther, listOther, err := GerritCommits(r, hashOther)
+	mapOther, listOther, err := GerritCommits(commit)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// open .gerrit-cherry-ignore for explicit skipped Change-Ids
+	pwd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	dir, err := filepath.Abs(pwd)
+	for {
+		filename := dir + string(os.PathSeparator) + filenameGerritCherryIgnore
+		if _, err := os.Stat(filename); !os.IsNotExist(err) {
+			file, err := os.Open(filename)
+			if err == nil {
+				defer file.Close()
+				scanner := bufio.NewScanner(file)
+				for scanner.Scan() {
+					line := strings.TrimSpace(scanner.Text())
+					if len(line) == 0 { // empty line
+						continue
+					} else if strings.HasPrefix(line, "#") { // comment
+						continue
+					}
+
+					commit := &Commit{}
+
+					// do not care about Commit.Title
+					n, err := fmt.Sscanf(line, "%41s %40s", &commit.ChangeId, &commit.CommitId)
+					if err == nil && n == 2 {
+						mapHEAD[commit.ChangeId] = commit
+					}
+				}
+			}
+			break
+		}
+		parentDir := filepath.Dir(dir)
+		if parentDir == dir {
+			// file ".gerrit-cherry-ignore" not found in system root directory
+			break
+		}
+		dir = parentDir
+	}
 
 	// remove commits that already in HEAD
 	for k, _ := range mapHEAD {
@@ -108,9 +134,7 @@ func main() {
 	for e := listOther.Front(); e != nil; e = e.Next() {
 		var changeId string = e.Value.(string)
 		if c := mapOther[changeId]; c != nil {
-			PrintCommit(c, changeId)
+			PrintCommit(c)
 		}
-		
 	}
-	
 }
