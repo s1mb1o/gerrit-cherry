7e80c1f1d6db88fc1daa8ebd72a234872d281721
Alexander Shmelev <ashmelev@smartlabs.tv>
2019-10-07 17:22:36 +0300

    Parse git with git-go

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"os"
 	"strings"
-	//	"time"
+	"time"
+
+	"container/list"
 
 	"gopkg.in/src-d/go-git.v4"
 	. "gopkg.in/src-d/go-git.v4/_examples"
@@ -13,32 +15,54 @@ import (
 	"gopkg.in/src-d/go-git.v4/plumbing/object"
 )
 
+// Gerrit change-Id prefix in commit message
 const kChangeId = "Change-Id: "
+// Limit log history to speedup 
+const startTime = "2019-01-01T00:00:00Z"
 
-func GerritCommits(r *git.Repository, hash plumbing.Hash) (map[string]object.Commit, error) {
-	var m map[string]object.Commit = make(map[string]object.Commit)
+func GerritCommits(r *git.Repository, hash plumbing.Hash) (map[string]*object.Commit, *list.List, error) {
+	// change-Id -> commit object
+	var m map[string]*object.Commit = make(map[string]*object.Commit)
+	// ordered changeId
+	var l *list.List = list.New() 
+
 	var err error
+
+	since, err := time.Parse(time.RFC3339, startTime)
+	CheckIfError(err)
+
 
 	cIter, err := r.Log(&git.LogOptions{From: hash})
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	err = cIter.ForEach(func(c *object.Commit) error {
-		scanner := bufio.NewScanner(strings.NewReader(fmt.Sprintf("%v", c)))
+		if since.After(c.Committer.When) {
+			return plumbing.ErrInvalidType
+		}
+
+		scanner := bufio.NewScanner(strings.NewReader(c.Message))
 		for scanner.Scan() {
-			line := strings.TrimSpace(scanner.Text())
+			line := scanner.Text()
 			if strings.HasPrefix(line, kChangeId) {
 				changeId := strings.TrimPrefix(line, kChangeId)
-
-				m[changeId] = *c
+				m[changeId] = c
+				l.PushBack(changeId)
 				break
+
 			}
 		}
 		return nil
 	})
 
-	return m, nil
+	return m, l, nil
+}
+
+func PrintCommit(c *object.Commit, changeId string) {
+	short := strings.SplitN(c.Message, "\n", 2)[0]
+
+	fmt.Printf("%v %v %v\n", changeId, c.Hash, short)
 }
 
 func main() {
@@ -51,8 +75,6 @@ func main() {
 	refHEAD, err := r.Head()
 	CheckIfError(err)
 	hashHEAD := refHEAD.Hash()
-	mapHEAD, err := GerritCommits(r, hashHEAD)
-	CheckIfError(err)
 
 	var refOther *plumbing.Reference
 	// trying reference name as is
@@ -61,7 +83,6 @@ func main() {
 		// expanding short reference name to full name
 		for _, format := range plumbing.RefRevParseRules {
 			full := fmt.Sprintf(format, commit)
-			fmt.Println(full)
 			refOther, err = r.Reference(plumbing.ReferenceName(full), true)
 			if err == nil {
 				break
@@ -70,13 +91,26 @@ func main() {
 	}
 	CheckIfError(err)
 
+	// read HEAD
+	mapHEAD, _, err := GerritCommits(r, hashHEAD)
+	CheckIfError(err)
+
+	// read other
 	hashOther := refOther.Hash()
-	mapOther, err := GerritCommits(r, hashOther)
+	mapOther, listOther, err := GerritCommits(r, hashOther)
 
 	// remove commits that already in HEAD
 	for k, _ := range mapHEAD {
 		delete(mapOther, k)
 	}
 
-	fmt.Println(mapOther)
+	// print left commits in other branch
+	for e := listOther.Front(); e != nil; e = e.Next() {
+		var changeId string = e.Value.(string)
+		if c := mapOther[changeId]; c != nil {
+			PrintCommit(c, changeId)
+		}
+		
+	}
+	
 }
